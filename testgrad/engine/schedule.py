13d9300733e66e37744d1657d30a25d310eed5fe
George Hotz <geohot@gmail.com>
2025-06-11 17:47:21 -0700

    add scheduler

--- testgrad/engine/schedule.py
+++ testgrad/engine/schedule.py
@@ -1,3 +1,44 @@
-ScheduleItem = None
-def create_schedule_with_vars(s):
-  return [], {}
+from typing import cast
+from collections import deque, defaultdict
+from dataclasses import dataclass, field
+from testgrad.helpers import Metadata
+from testgrad.device import Buffer
+from testgrad.uop.ops import UOp, Variable, Ops
+
+@dataclass(frozen=True)
+class ScheduleItem:
+  ast: UOp
+  bufs: tuple[Buffer, ...]
+  metadata: tuple[Metadata, ...] = ()
+  fixedvars: dict[Variable, int] = field(default_factory=dict)
+
+def create_schedule_with_vars(sched_sink:UOp):
+  # construct the KERNEL children graph based on assigns
+  children: defaultdict[UOp, list[UOp]] = defaultdict(list)
+  in_degree: dict[UOp, int] = {}
+  for u in sched_sink.toposort():
+    if u.op is not Ops.STORE: continue  # anything that's not a STORE doesn't write a kernel, so we can skip
+    k = u.src[1]
+    in_degree.setdefault(k, 0)
+    for s in k.src:
+      if s.op is Ops.STORE:
+        children[s.src[1]].append(k)
+        in_degree[k] += 1
+      elif s.op is Ops.BUFFER:
+        pass  # a BUFFER is already realized, nothing to do here
+      else:
+        raise RuntimeError(f"input to kernel must be STORE or BUFFER, not {s.op}")
+
+  # linearize KERNEL UOps into ScheduleItems in BFS order
+  queue = deque(k for k,v in in_degree.items() if v == 0)
+  schedule: list[ScheduleItem] = []
+  var_vals: dict[Variable, int] = {}
+  while queue:
+    k = queue.popleft()
+    ubufs = tuple(s.buf_uop.buffer for s in k.src)
+    schedule.append(ScheduleItem(k.arg.ast, cast(tuple[Buffer, ...], ubufs)))
+    for x in children[k]:
+      in_degree[x] -= 1
+      if in_degree[x] == 0: queue.append(x)
+
+  return schedule, var_vals
